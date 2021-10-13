cad50898024c8e9f9a3d213ebd5d29ee72b5db0c
Yegor Shytikov <egorshitiov@gmail.com>
2021-10-13 13:20:38 -0700

    Magento Fork initial commit

--- Magento_Theme/web/js/theme.js
+++ Magento_Theme/web/js/theme.js
@@ -17,13 +17,6 @@ define([
     });
 
     $('.panel.header > .header.links').clone().appendTo('#store\\.links');
-    $('#store\\.links li a').each(function () {
-        var id = $(this).attr('id');
-
-        if (id !== undefined) {
-            $(this).attr('id', id + '_mobile');
-        }
-    });
 
     keyboardHandler.apply();
 });
